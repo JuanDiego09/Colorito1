2ac3e09fc112d31a24964c8b118064a10df651ef
wordlskills <juandiego1918@gmail.com>
2018-08-16 10:36:34 -0500

    juan

---
+++ app/src/main/java/com/example/wordlskills/colorito1/JuegoDefectoActivity.java
@@ -0,0 +1,13 @@
+package com.example.wordlskills.colorito1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JuegoDefectoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_defecto);
+    }
+}
--- app/src/main/java/com/example/wordlskills/colorito1/MainActivity.java
+++ app/src/main/java/com/example/wordlskills/colorito1/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.wordlskills.colorito1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button jugar,ajuste,lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jugar=findViewById(R.id.btnjugar);
+        ajuste=findViewById(R.id.btnAjuste);
+        lista=findViewById(R.id.btnPuntajes);
+
+
+        jugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,JuegoDefectoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
